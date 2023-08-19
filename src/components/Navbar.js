69862b4ea2575de9cb6f261df3ff38cedccac187
Gitishan Biswal <gitishan20@gmail.com>
2023-08-19 17:10:08 +0530

    2

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,18 +1,31 @@
 import { Component } from "react";
 import "./NavbarSyle.css";
- 
-class Navbar extends Component{
-    render() {
-        return (
-            <nav className="NavbarItems">
-                <h1 className="navbar-logo">WayTrip</h1>
-                <ul className="nav-menu">
-                    <li>
-                        <link><i class="fa-solid fa-house"></i>Home</link>
-                    </li>
-                </ul>
-            </nav>
-        )
-};
+import { Link } from "react-router-dom";
+import { MenuItems } from "./MenuItems";
+
+
+class Navbar extends Component {
+  render() {
+    return (
+      <nav className="NavbarItems">
+      
+        <h1 className="navbar-logo">Travist</h1>
+        
+
+        <ul className="nav-menu">
+          {MenuItems.map((item, index) => {
+            return (
+              <li key={index}>
+                <a  className={item.cName}href="/">
+                  <i class={item.icon}></i>{item.title}
+                </a>
+              </li>
+            );
+          })}
+           
+        </ul>
+      </nav>
+    );
+  }
 }
 export default Navbar;
